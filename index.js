7473a8d2dffab3844c7dfa6db3876d4f9f26a20c
GunesAtik <guenes_atik@hotmail.de>
2021-01-16 22:26:05 +0100

    route listed endpoints

--- index.js
+++ index.js
@@ -35,6 +35,51 @@ app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: movie_api });
 });
 
+// Get a list of all movies
+app.get('/movies', (req, res) => {
+  res.send('Successful GET request returning data on all movies');
+});
+
+// Get data about a single movie by title
+app.get('/movies/:title', (req,res) => {
+  res.json(topMovies);
+});
+
+// Return genre of movie by title
+app.get('/movies/genres/:title', (req,res) => {
+  res.send('Successful GET request returning data on genre');
+});
+
+// Return director of movie by title
+app.get('/movies/directors/:title', (req,res) => {
+  res.send('Successful GET request returning data on director');
+});
+
+// Allow new users to register
+app.post('/users', (req,res) => {
+  res.send('Successful POST request registering new user');
+});
+
+// Allow users to update their user info
+app.put('/users/:username', (req,res) => {
+  res.send('Successful PUT request updating info for user');
+});
+
+// Allow users to add a movie to their list of favourites
+app.post('/users/:username/movies/', (req,res) => {
+  res.send('Successful POST request adding movie to favorite movie list');
+});
+
+// Allow users to remove a movie from their favourites
+app.delete('/users/:username/movies/', (req,res) => {
+  res.send('Successful DELETE request removing movie from favorite movie list of user');
+});
+
+// Allow users to deregister
+app.delete('/users/:username', (req,res) => {
+  res.send('Successful DELETE request removing user from database');
+});
+
 app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
