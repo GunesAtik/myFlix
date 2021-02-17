8a527a3ddd3532b0c37e07d34b170265f82b9397
Your Name <you@example.com>
2021-02-17 16:45:20 +0100

    add validation to endpoint

--- index.js
+++ index.js
@@ -98,11 +98,32 @@ app.get('/movies/directors/:Name', passport.authenticate('jwt', { session: false
   Email: String,
   Birthday: Date
 }*/
-app.post('/users', (req, res) => {
+app.post('/users',
+// Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
+[
+check('Username', 'Username is required').isLength({min: 5}),
+check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+check('Password', 'Password is required').not().isEmpty(),
+check('Email', 'Email does not appeae to be valid').isEmail()
+] (req, res) => {
+
+// check the validation object for errors
+let errors = validationResult(req);
+
+if (!errors.isEmpty()) {
+  return res.status(422).json({ errors: errors.array() });
+}
+
   let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
+  // Search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
+  //If the user is found, send a response that it already exists
         return res.status(400).send(req.body.Username + 'already exists');
       } else {
         Users
