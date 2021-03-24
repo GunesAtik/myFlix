9fdc561d7c082326c21b0d54cf17a6f89696a3ec
„guenes <guenes_atik@hotmail.de>
2021-03-24 17:09:54 +0100

    updated allowed origins

--- index.js
+++ index.js
@@ -16,7 +16,30 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 const cors = require('cors');
-app.use(cors());
+
+let allowedOrigins = [
+  "http://localhost:1234"
+];
+/**
+ * CORS blocks requests from origins not listed in 'allowedOrigins'
+ */
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log(origin, allowedOrigins.indexOf(origin) === -1);
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application does not allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 const { check, validationResult } = require('express-validator');
 
 /*mongoose.connect('mongodb://localhost:27017/myFlixDB',
